1b060b9a7e12a25efc82a90a2f36d665def67990
Isaac Stern <istern@canarylabs.com>
2018-09-04 14:03:06 -0400

    Added .net client code example to retrieve data

---
+++ Samples/Data Retrieval/.NET Client/ConnectionWrapper.cs
@@ -0,0 +1,198 @@
+﻿using CanaryWebServiceHelper;
+using CanaryWebServiceHelper.HistorianWebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadData
+{
+    class ConnectionException : Exception
+    {
+        public ConnectionException(string message = "Unable to connect to endpoint.") : base(message) { }
+    }
+
+    class ConnectionWrapper : IDisposable
+    {
+        #region Private Members
+
+        private bool _isConnected;
+        private HistorianWebServiceClient _client;
+        private HWS_ConnectionType _connectionType;
+        private string _host;
+        private int? _port;
+        private string _appString;
+        private string _userString;
+        private string _user;
+        private string _password;
+        private int _cci; // client connection id
+
+        #endregion
+
+        #region Private Methods
+
+        private bool Connect(string identity = null)
+        {
+            if (_isConnected)
+                return true;
+
+            string host = _host;
+            if (_port.HasValue)
+                host = $"{host}:{_port.Value}";
+            if (!string.IsNullOrEmpty(identity))
+                host = $"{host};{identity}";
+
+            try
+            {
+                HWS_ConnectionHelper.WebServiceConnect(
+                    _connectionType,
+                    host,
+                    _appString,
+                    _userString,
+                    _user,
+                    _password,
+                    out _client,
+                    out _cci);
+
+                _isConnected = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (identity == null && _connectionType == HWS_ConnectionType.NetTcp_Username)
+                {
+                    // try to parse dns identity from exception
+                    // necessary when connecting to username endpoint
+                    if (TryGetIdentity(ex, out identity))
+                        return Connect(identity);
+                }
+            }
+
+            _isConnected = false;
+            return false;
+        }
+
+        private bool Reconnect()
+        {
+            if (_isConnected)
+            {
+                Disconnect();
+                Connect();
+                return true;
+            }
+            return false;
+        }
+
+        private void Disconnect()
+        {
+            try
+            {
+                if (_client != null)
+                    _client.ReleaseClientConnectId(_appString, _userString, _cci);
+                _client.Close();
+            }
+            catch
+            {
+            }
+            finally
+            {
+                _isConnected = false;
+                _client = null;
+                _cci = 0;
+            }
+        }
+
+        private bool TryGetIdentity(Exception ex, out string identity)
+        {
+            string message = ex.Message;
+            string[] split = message.Split('\'');
+            identity = (split.Length >= 4) ? split[3] : null;
+            return !string.IsNullOrEmpty(identity);
+        }
+
+        private void ValidateConnectionParameters()
+        {
+            if (!_isConnected)
+            {
+                if (_connectionType == HWS_ConnectionType.Https_Username || _connectionType == HWS_ConnectionType.NetTcp_Username)
+                {
+                    if (string.IsNullOrEmpty(_user) || string.IsNullOrEmpty(_password))
+                        throw new ConnectionException("Credentials must be set when using a username endpoint.");
+                }
+                else if (!string.IsNullOrEmpty(_user) || !string.IsNullOrEmpty(_password))
+                    throw new ConnectionException("Endpoint does not use credentials. Do not set them.");
+            }
+        }
+
+        #endregion
+
+        #region Constructor/Destructor
+
+        public ConnectionWrapper(string host, HWS_ConnectionType connectionType, string application = null, string clientDescription = null)
+        {
+            _host = host;
+            _connectionType = connectionType;
+            _appString = string.IsNullOrEmpty(application) ? "Read Data Example" : application;
+            _userString = string.IsNullOrEmpty(clientDescription) ? Environment.MachineName : clientDescription;
+            _user = null;
+            _password = null;
+            _cci = 0;
+        }
+
+        ~ConnectionWrapper()
+        {
+            Disconnect();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string Username
+        {
+            set { _user = value; }
+        }
+
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        public int Port
+        {
+            set { _port = value; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Run(Action<HistorianWebServiceClient, int> action)
+        {
+            try
+            {
+                ValidateConnectionParameters();
+                if (Connect())
+                    action(_client, _cci);
+            }
+            catch
+            {
+                if (Reconnect())
+                    action(_client, _cci);
+                else
+                    throw;
+            }
+
+            if (!_isConnected)
+                throw new ConnectionException();
+        }
+
+        public void Dispose()
+        {
+            Disconnect();
+        }
+
+        #endregion
+    }
+}
---
+++ Samples/Data Retrieval/.NET Client/Program.cs
@@ -0,0 +1,275 @@
+﻿using CanaryWebServiceHelper;
+using CanaryWebServiceHelper.HistorianWebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResizeWindow();
+
+            string host = "localhost";
+            HWS_ConnectionType connectionType = HWS_ConnectionType.NetTcp_Windows;
+            using (ConnectionWrapper connection = new ConnectionWrapper(host, connectionType)
+            {
+                //// use credentials when connecting to username endpoint
+                //Username = "name",
+                //Password = "secret"
+            })
+            {
+                try
+                {
+                    // handles reconnect logic
+                    connection.Run((client, cci) =>
+                    {
+                        // test version
+                        Version(client);
+
+                        // browse nodes/tags
+                        BrowseNodes(client, out string nodePath);
+                        BrowseTags(client, nodePath, out List<string> tags);
+
+                        if (tags != null && tags.Count > 0)
+                        {
+                            DateTime endTime = DateTime.Now;
+                            DateTime startTime = endTime.Subtract(TimeSpan.FromHours(1));
+                            List<string> tagSubset = tags.Take(5).ToList();
+
+                            // read data
+                            GetCurrentValue(client, cci, tagSubset);
+                            GetRawData(client, cci, tagSubset, startTime, endTime);
+                            GetAvailableAggregates(client);
+                            GetProcessedData(client, cci, tagSubset, startTime, endTime);
+                        }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static private void ResizeWindow()
+        {
+            try
+            {
+                // width in characters
+                Console.WindowWidth = 150;
+                Console.WindowHeight = 50;
+            }
+            catch
+            {
+                // ignore error
+            }
+        }
+
+        static private void Version(HistorianWebServiceClient client)
+        {
+            string version = client.WebServiceVersion();
+            Console.WriteLine($"Version: {version}");
+        }
+
+        static private void BrowseNodes(HistorianWebServiceClient client, out string nodePath)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Browse Nodes...");
+
+            nodePath = "ROOT"; // base path
+            while (true)
+            {
+                HWSBrowseNode result = client.Browse(nodePath, false);
+                if (result != null)
+                {
+                    if (!string.IsNullOrEmpty(result.errMsg))
+                        Console.WriteLine($"Browse Nodes Error: {result.errMsg}");
+                    if (result.children == null || result.children.Length == 0)
+                        Console.WriteLine($"\"{nodePath}\" has 0 child nodes...");
+                    else
+                    {
+                        // browse deeper into first child node
+                        HWSBrowseInfo firstNode = result.children[0];
+                        Console.WriteLine($"\"{nodePath}\" path has {result.children.Length} child nodes...");
+                        nodePath = firstNode.idPath;
+                        continue;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        static private void BrowseTags(HistorianWebServiceClient client, string nodePath, out List<string> tags)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Browse All Tags...");
+
+            tags = new List<string>();
+            string search = "";
+            while (true)
+            {
+                int maxCount = 1000;
+                bool includeSubNodes = true;
+                bool includeProperties = false;
+                HWSBrowseTags result = client.BrowseTags(nodePath, search, maxCount, includeSubNodes, includeProperties);
+                if (result != null)
+                {
+                    if (!string.IsNullOrEmpty(result.errMsg))
+                        Console.WriteLine($"Browse Tags Error: {result.errMsg}");
+                    else if (result.tagNames != null)
+                    {
+                        Console.WriteLine($"Retrieved {result.tagNames.Length} tags from path \"{nodePath}\"...");
+
+                        tags.AddRange(result.tagNames.Select((partialTagName) =>
+                        {
+                            // build full tag path
+                            return $"{nodePath}.{partialTagName}";
+                        }));
+
+                        if (result.moreDataAvailable)
+                        {
+                            // use search context as continuation point to get more tags
+                            search = result.searchContext;
+                            continue;
+                        }
+                    }
+                }
+
+                break;
+            }
+        }
+
+        static private void GetCurrentValue(HistorianWebServiceClient client, int cci, List<string> tags)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Getting Current Values...");
+
+            foreach (string fullTag in tags)
+            {
+                string[] split = fullTag.Split('.');
+                string historian = split[0];
+                string dataset = null;
+                string partialTag = string.Join(".", split.Skip(1));
+
+                HWSTagCurrentValue result = client.GetTagCurrentValue(historian, dataset, new string[] { partialTag }, cci)[0];
+                if (result != null)
+                {
+                    Console.WriteLine($"\"{fullTag}\" =>");
+                    Console.WriteLine($"\tTime = {result.timeStamp}");
+                    Console.WriteLine($"\tValue = {result.value}");
+                    Console.WriteLine($"\tQuality = {client.GetReadableQuality(result.quality)}");
+                }
+            }
+        }
+
+        static private void GetRawData(HistorianWebServiceClient client, int cci, List<string> tags, DateTime startTime, DateTime endTime)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Getting Raw Data...");
+
+            foreach (string fullTag in tags)
+            {
+                Console.WriteLine($"\"{fullTag}\" =>");
+
+                string[] split = fullTag.Split('.');
+                string historian = split[0];
+                string partialTag = string.Join(".", split.Skip(1));
+
+                byte[] continuationPoint = null;
+                while (true)
+                {
+                    HWSTagRequest3[] request = new HWSTagRequest3[]
+                    {
+                        new HWSTagRequest3()
+                        {
+                            endTime = endTime,
+                            startTime = startTime,
+                            tagName = partialTag,
+                            continuationPoint = continuationPoint
+                        }
+                    };
+
+                    int maxCount = 1000;
+                    bool returnBounds = true;
+                    bool returnAnnotation = false;
+                    HWSTagData3 result = client.GetRawTagData3(historian, request, maxCount, returnBounds, returnAnnotation, cci)[0];
+                    if (result != null)
+                    {
+                        if (!string.IsNullOrEmpty(result.errMsg))
+                            Console.WriteLine($"\tGet Raw Data Error: {result.errMsg}");
+                        else if (result.tvqs != null)
+                        {
+                            Console.WriteLine($"\tRetrieved {result.tvqs.Length} raw values...");
+                            if (result.continuationPoint != null && result.continuationPoint.Length > 0)
+                            {
+                                // get more data
+                                continuationPoint = result.continuationPoint;
+                                continue;
+                            }
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        static private void GetAvailableAggregates(HistorianWebServiceClient client)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Getting available aggregates for processed data...");
+
+            HWSAggregateDefinition[] aggregates = client.GetAggregateList2();
+            if (aggregates != null)
+            {
+                foreach (HWSAggregateDefinition aggregate in aggregates)
+                    Console.WriteLine($"{aggregate.aggregateName} => {aggregate.aggregateDescription}");
+            }
+        }
+
+        static private void GetProcessedData(HistorianWebServiceClient client, int cci, List<string> tags, DateTime startTime, DateTime endTime)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Getting Processed Data...");
+
+            TimeSpan aggregateInterval = TimeSpan.FromMinutes(1);
+            foreach (string fullTag in tags)
+            {
+                Console.WriteLine($"\"{fullTag}\" =>");
+
+                string[] split = fullTag.Split('.');
+                string historian = split[0];
+                string partialTag = string.Join(".", split.Skip(1));
+
+                HWSTagProcessedRequest2[] request = new HWSTagProcessedRequest2[]
+                {
+                    new HWSTagProcessedRequest2()
+                    {
+                        aggregateId = HWSAggregate.TimeAverage2,
+                        tagName = partialTag
+                    }
+                };
+
+                bool returnAnnotations = false;
+                HWSTagProcessedData2 result = client.GetProcessedTagData2(historian, request, startTime, endTime, aggregateInterval, returnAnnotations, cci)[0];
+                if (result != null)
+                {
+                    if (!string.IsNullOrEmpty(result.errMsg))
+                        Console.WriteLine($"\tGet Processed Data Error: {result.errMsg}");
+                    else if (result.tvqs != null)
+                        Console.WriteLine($"\tRetrieved {result.tvqs.Length} processed values...");
+                }
+            }
+        }
+    }
+}
